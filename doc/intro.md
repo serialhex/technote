bf6f09b7c086944ee89bd92f3050e5888469cc49
Justin Patera <serialhex@gmail.com>
2013-06-19 08:22:08 -0400

    initial commit, starting anew

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to technote
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/technote/core_test.clj
@@ -0,0 +1,7 @@
+(ns technote.core-test
+  (:use clojure.test
+        technote.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
