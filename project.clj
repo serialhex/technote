317ac2c7ba18e2f9516cdedf2d2a6f66d7debf47
Justin Patera <serialhex@gmail.com>
2013-06-19 08:50:23 -0400

    getting set up

--- project.clj
+++ project.clj
@@ -3,4 +3,32 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+
+  ;; CLJ source code path
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [hiccup "1.0.3"]
+                 [compojure "1.1.5"]]
+
+  ;; lein-cljsbuild plugin to build a CLJS project
+  :plugins [[lein-cljsbuild "0.3.2"]
+
+            ;; ring plugin
+            [lein-ring "0.8.5"]]
+
+  :ring {:handler technote.core/handler}
+
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/technote.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]}))
