549640b90465acffffac7e4c2af22104eb8b8b4d
Justin Patera <serialhex@gmail.com>
2013-06-25 22:23:00 -0400

    fixing db for heroku

--- src/clj/technote/database.clj
+++ src/clj/technote/database.clj
@@ -1,12 +1,12 @@
 (ns technote.database
-  (:require [monger.core       :as mc   :refer [connect! set-db! get-db]]
+  (:require [monger.core       :as mc   :refer [connect-via-uri! set-db! get-db]]
             [monger.collection :as coll :refer [insert-and-return insert-batch]]
             [monger.query      :as q])
   (:import [org.bson.types ObjectId]))
 
 ; [:company :name :street :city :zip :phone :problems]
 
-(connect!)
+(connect-via-uri! (get (System/getenv) "MONGOHQ_URL" "mongodb://127.0.0.1/tech-test"))
 ; set default db - the test one!
 (set-db! (get-db "tech-test"))
 
