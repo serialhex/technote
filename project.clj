1a5a2828e222456dbcef9a19411845e284769f55
Justin Patera <serialhex@gmail.com>
2013-06-25 22:35:54 -0400

    this is getting annoying...

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.2.0"
 
   ;; CLJ source code path
   :source-paths ["src/clj"]
