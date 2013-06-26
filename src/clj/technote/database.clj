f344982797e153309c40416650e900517d910b6d
Justin Patera <serialhex@gmail.com>
2013-06-25 22:47:34 -0400

    ffs

--- src/clj/technote/database.clj
+++ src/clj/technote/database.clj
@@ -6,7 +6,7 @@
 
 ; [:company :name :street :city :zip :phone :problems]
 
-(connect-via-uri! (get (System/getenv) "MONGOHQ_URL" "mongodb://localhost/tech-test"))
+(connect-via-uri! (get (System/getenv) "MONGOHQ_URL"))
 ; set default db - the test one!
 ; (set-db! (get-db "tech-test"))
 
