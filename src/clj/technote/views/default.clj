0ed60ebe82f5e57c6646579a1c1d4765aa00a9e2
Justin Patera <serialhex@gmail.com>
2013-09-04 14:51:15 -0400

    small changes: defhtml

--- src/clj/technote/views/default.clj
+++ src/clj/technote/views/default.clj
@@ -1,26 +1,26 @@
 (ns technote.views.default
   (:require [hiccup.page :refer [html5]]
+            [hiccup.def  :refer [defhtml]]
             [hiccup.core :refer [html]]))
 
-(defn default-page [& body]
-  (html5 [:html
-            [:head
-              [:meta {:charset "utf-8"}]
-              [:title "Technote"]
-              [:link {:rel "stylesheet" :href "/css/normalize.css"}]
-              [:link {:rel "stylesheet" :href "/css/flat-ui.css"}]
-              ; [:link {:rel "stylesheet" :href "/css/styles.css"}]
-              [:link {:rel "stylesheet" :href "/css/technote.css"}]
-              ]
-            [:body
-              [:ul#sidebar
-                [:li [:a {:href "/login"} "login"]]
-                [:li [:a {:href "/workorder"} "workorder"]]
-                [:li [:a {:href "/workorder/new"} "new workorder"]]
-                [:li [:a {:href "/customer"} "customer"]]]
-              [:div#main
-                (html body)]]
-              [:script {}]]))
+(defhtml default-page [& body]
+  [:html
+    [:head
+      [:meta {:charset "utf-8"}]
+      [:title "Technote"]
+      [:link {:rel "stylesheet" :href "/css/normalize.css"}]
+      [:link {:rel "stylesheet" :href "/css/flat-ui.css"}]
+      ; [:link {:rel "stylesheet" :href "/css/styles.css"}]
+      [:link {:rel "stylesheet" :href "/css/technote.css"}]
+      ]
+    [:body
+      [:ul#sidebar
+        [:li [:a {:href "/login"} "login"]]
+        [:li [:a {:href "/workorder"} "workorder"]]
+        [:li [:a {:href "/workorder/new"} "new workorder"]]
+        [:li [:a {:href "/customer"} "customer"]]]
+      [:div#main body]]
+      [:script {}]])
 
 (defn four-oh-4 [x]
   (default-page
